d6ca57886ccfd163ff8059417faf18020366e9f3
Slava-B22 <slava.grig@gmail.com>
2021-05-29 10:48:01 -0400

    AddressList

---
+++ src/Day47_constructors/Student.java
@@ -0,0 +1,11 @@
+package Day47_constructors;
+
+public class Student {
+    public Student (){
+        // no args constructor
+        System.out.println("No args constructor");
+    }
+    public Student (String name) {
+        System.out.println("name parametr constructor | name = " + name);
+    }
+}
